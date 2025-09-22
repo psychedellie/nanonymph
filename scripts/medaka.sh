aae7f9a7849dbe9869ca51f65cda60504b249f5f
psychedellie <ellitheofili@gmail.com>
2025-09-22 11:53:08 +0300

    Medaka script issue with model resolved

--- scripts/medaka.sh
+++ scripts/medaka.sh
@@ -2,10 +2,11 @@ np_raw_file=$1
 output_dir=$2
 assembly=$3
 threads=$4
+model=$5
 
 if [ -d $output_dir ]; then
         echo "Deleting existing folder: $output_dir"
         rm -rf $output_dir
 fi
 
-micromamba run -n medaka medaka_consensus -i $np_raw_file -d $assembly -o $output_dir -t $threads --bacteria
+micromamba run -n medaka medaka_consensus -i $np_raw_file -d $assembly -o $output_dir -t $threads --bacteria -m $model
